b573ae3b328e21235e07cf8527247bbbc1c43510
乾太 Kantai <kantai.developer@gmail.com>
2019-10-31 23:30:26 +0800

    把 META 的一些參數移到 env 當中

--- resources/lang/zh-TW/meta.php
+++ resources/lang/zh-TW/meta.php
@@ -9,19 +9,19 @@ return [
     */
 
     'og' => [
-        'type' => 'website',
-        'title' => '不要誤會，我不是針對你，我是說在座的各位，都是純靠北工程師。',
-        'image' => 'https://i.imgur.com/klfOphB.png',
-        'description' => '發源自臉書──全台最大工程師廢文社群 https://kaobei.engineer',
-        'keyword' => '純靠北工程師, 發源自臉書, 全台最大工程師廢文社群, 濁水溪以南最大, 濁水溪以北最大, 講幹話, 發廢文, 罵老闆, 婊同事, 我看你是不夠敏捷喔',
+        'type' => env('META_OG_TYPE', 'website'),
+        'title' => env('META_OG_TITLE'),
+        'image' => env('META_OG_IMAGE'),
+        'description' => env('META_OG_DESCRIPTION'),
+        'keyword' => env('META_OG_KEYWORD'),
     ],
 
     'facebook' => [
-        'app_id' => '137466300263351',
+        'app_id' => env('META_FACEBOOK_APP_ID'),
     ],
 
     'google' => [
-        'site_verification' => 'h34NQpMQBOKASrlYhsf6Jvp4XyhcGsG0aQI0NIV1MHc',
+        'site_verification' => env('META_GOOGLE_SITE_VERIFICATION'),
     ],
 
     'twitter' => [
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -87,13 +87,13 @@
         @include('includes.partials.ga')
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-89952948-2"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_ID') }}"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'UA-89952948-2');
+            gtag('config', '{{ env('GOOGLE_ANALYTICS_ID') }}');
         </script>
     </body>
 </html>
