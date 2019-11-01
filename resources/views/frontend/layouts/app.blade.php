289718dafe6494bd70d322ed1e48eff4d93c1120
乾太 Kantai <kantai.developer@gmail.com>
2019-11-01 18:19:15 +0800

    使用 Laravel Boilerplate 本身擁有的 Google Analytics

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -85,15 +85,5 @@
         @stack('after-scripts')
 
         @include('includes.partials.ga')
-
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_ID') }}"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '{{ env('GOOGLE_ANALYTICS_ID') }}');
-        </script>
     </body>
 </html>
