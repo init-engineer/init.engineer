b2ceffa71b243aa1d09156063249dee75e93a13d
Kantai235 <kantai.developer@gmail.com>
2019-11-07 10:50:08 +0800

    首頁新增發表文章連結

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -14,7 +14,7 @@
                 <div class="carousel-caption text-left mb-5 pb-5 text-dark">
                     <h1 class="text-black-stroke display-3">純靠北工程師</h1>
                     <p class="lead">發源自臉書──全台最大工程師廢文社群</p>
-                    <a class="btn btn-warning btn-lg shake-slow shake-constant shake-constant--hover" href="#" role="button">開始去發廢文 <i class="fas fa-poop"></i></a>
+                    <a class="btn btn-warning btn-lg shake-slow shake-constant shake-constant--hover" href="{{ route('frontend.social.cards.create') }}" role="button">開始去發廢文 <i class="fas fa-poop"></i></a>
                 </div>
             </div>
         </div>
