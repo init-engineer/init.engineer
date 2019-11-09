cc4079131c42c929925f9f73ab209caa5fe19696
Kantai235 <kantai.developer@gmail.com>
2019-11-09 13:15:25 +0800

    社群文章發表寫成列隊 Queue 去呼叫 Service

---
+++ app/Jobs/Social/MediaCards/FacebookPrimaryPublish.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Jobs\Social\MediaCards;
+
+use App\Models\Social\Cards;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Services\Socials\MediaCards\FacebookPrimaryService;
+
+/**
+ * Class FacebookPrimaryPublish.
+ */
+class FacebookPrimaryPublish implements ShouldQueue
+{
+    use Queueable,
+        Dispatchable,
+        SerializesModels,
+        InteractsWithQueue;
+
+    /**
+     * @var Cards
+     */
+    protected $cards;
+
+    /**
+     * Determine the time at which the job should timeout.
+     *
+     * @return \DateTime
+     */
+    public function retryUntil()
+    {
+        return now()->addMinutes(6);
+    }
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Cards $cards)
+    {
+        $this->cards = $cards;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(FacebookPrimaryService $service)
+    {
+        $service->publish($this->cards);
+    }
+}
---
+++ app/Jobs/Social/MediaCards/TwitterPrimaryPublish.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Jobs\Social\MediaCards;
+
+use App\Models\Social\Cards;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Services\Socials\MediaCards\TwitterPrimaryService;
+
+/**
+ * Class TwitterPrimaryPublish.
+ */
+class TwitterPrimaryPublish implements ShouldQueue
+{
+    use Queueable,
+        Dispatchable,
+        SerializesModels,
+        InteractsWithQueue;
+
+    /**
+     * @var Cards
+     */
+    protected $cards;
+
+    /**
+     * Determine the time at which the job should timeout.
+     *
+     * @return \DateTime
+     */
+    public function retryUntil()
+    {
+        return now()->addMinutes(6);
+    }
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Cards $cards)
+    {
+        $this->cards = $cards;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(TwitterPrimaryService $service)
+    {
+        $service->publish($this->cards);
+    }
+}
