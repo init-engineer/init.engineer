38a3b2572ef069a4c8d95201884574ffca70c31d
Kantai235 <kantai.developer@gmail.com>
2019-11-19 22:51:33 +0800

    調整 Footer

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -18,7 +18,7 @@
         <div class="row mt-3">
 
             <!-- Grid column -->
-            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                 <!-- Content -->
                 <h6 class="text-white font-weight-bold">關於本站</h6>
                 <hr class="bg-white accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 32px;">
@@ -32,53 +32,37 @@
             </div>
 
             <!-- Grid column -->
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
                 <!-- Links -->
-                <h6 class="text-white font-weight-bold">Contact</h6>
+                <h6 class="text-white font-weight-bold">聯絡我們</h6>
                 <hr class="bg-white accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 32px;">
-                <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                <p><i class="fas fa-envelope mr-3"></i> kantai.developer@gmail.com</p>
             </div>
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                 <!-- Links -->
                 <h6 class="text-white font-weight-bold">服務項目</h6>
                 <hr class="bg-white accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 32px;">
-                <p><a href="#!">喝ㄋㄟㄋㄟ</a></p>
-                <p><a href="#!">Become an Affiliate</a></p>
-                <p><a href="#!">Shipping Rates</a></p>
-                <p><a href="#!">Help</a></p>
+                <p><a href="{{ route('frontend.social.cards.index') }}">文章列表</a></p>
+                <p><a href="{{ route('frontend.social.cards.create') }}">發表文章</a></p>
             </div>
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-md-0 mb-4">
                 <!-- Links -->
                 <h6 class="text-white font-weight-bold">社群平台</h6>
                 <hr class="bg-white accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 32px;">
-                @if (app_imgur())
-                    <p><a href="https://kaobei.engineer"><img class="img-fluid rounded" src="https://i.imgur.com/Zl6Mrfs.png" alt="官方網站" /></a></p>
-                    <p><a href="https://www.facebook.com/kaobei.engineer"><img class="img-fluid rounded" src="https://i.imgur.com/zgwvd6x.png" alt="Facebook 測試機" /></a></p>
-                    <p><a href="https://www.facebook.com/init.kobeengineer"><img class="img-fluid rounded" src="https://i.imgur.com/3qOl69x.png" alt="Facebook 正式機" /></a></p>
-                    <p><a href="https://twitter.com/kaobei_engineer"><img class="img-fluid rounded" src="https://i.imgur.com/aPI28fr.png" alt="Twitter" /></a></p>
-                    <p><a href="https://www.plurk.com/kaobei_engineer"><img class="img-fluid rounded" src="https://i.imgur.com/SFcRaLN.png" alt="Plurk" /></a></p>
-                    <p><a href="https://www.instagram.com/kaobei.engineer"><img class="img-fluid rounded" src="https://i.imgur.com/6FgkF3R.png" alt="Instagram" /></a></p>
-                    <p><a href="https://kaobei-engineer.tumblr.com"><img class="img-fluid rounded" src="https://i.imgur.com/dffwOx9.png" alt="Tumblr" /></a></p>
-                    <p><a href="https://discord.gg/tPhnrs2"><img class="img-fluid rounded" src="https://i.imgur.com/tknHq2i.png" alt="Discord" /></a></p>
-                @else
-                    <p><a href="https://kaobei.engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/Zl6Mrfs.png') }}" alt="官方網站" /></a></p>
-                    <p><a href="https://www.facebook.com/kaobei.engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/zgwvd6x.png') }}" alt="Facebook 測試機" /></a></p>
-                    <p><a href="https://www.facebook.com/init.kobeengineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/3qOl69x.png') }}" alt="Facebook 正式機" /></a></p>
-                    <p><a href="https://twitter.com/kaobei_engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/aPI28fr.png') }}" alt="Twitter" /></a></p>
-                    <p><a href="https://www.plurk.com/kaobei_engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/SFcRaLN.png') }}" alt="Plurk" /></a></p>
-                    <p><a href="https://www.instagram.com/kaobei.engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/6FgkF3R.png') }}" alt="Instagram" /></a></p>
-                    <p><a href="https://kaobei-engineer.tumblr.com"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/dffwOx9.png') }}" alt="Tumblr" /></a></p>
-                    <p><a href="https://discord.gg/tPhnrs2"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/tknHq2i.png') }}" alt="Discord" /></a></p>
-                @endif
+                <p><a href="https://kaobei.engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/Zl6Mrfs.png') }}" alt="官方網站" /></a></p>
+                <p><a href="https://www.facebook.com/kaobei.engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/zgwvd6x.png') }}" alt="Facebook 測試機" /></a></p>
+                <p><a href="https://www.facebook.com/init.kobeengineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/3qOl69x.png') }}" alt="Facebook 正式機" /></a></p>
+                <p><a href="https://twitter.com/kaobei_engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/aPI28fr.png') }}" alt="Twitter" /></a></p>
+                <p><a href="https://www.plurk.com/kaobei_engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/SFcRaLN.png') }}" alt="Plurk" /></a></p>
+                <p><a href="https://www.instagram.com/kaobei.engineer"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/6FgkF3R.png') }}" alt="Instagram" /></a></p>
+                <p><a href="https://kaobei-engineer.tumblr.com"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/dffwOx9.png') }}" alt="Tumblr" /></a></p>
+                <p><a href="https://discord.gg/tPhnrs2"><img class="img-fluid rounded" src="{{ asset('img/frontend/banner/tknHq2i.png') }}" alt="Discord" /></a></p>
             </div>
             <!-- Grid column -->
 
