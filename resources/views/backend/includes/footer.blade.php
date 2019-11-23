5eea679e241c8d88b40adc787c5ac33009614e39
Kantai235 <kantai.developer@gmail.com>
2019-11-24 02:24:49 +0800

    後台頁面修改 Footer 內容

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,10 +1,6 @@
 <footer class="app-footer">
     <div>
-        <strong>@lang('labels.general.copyright') &copy; {{ date('Y') }}
-            <a href="http://laravel-boilerplate.com">
-                @lang('strings.backend.general.boilerplate_link')
-            </a>
-        </strong> @lang('strings.backend.general.all_rights_reserved')
+        @lang('strings.backend.general.all_rights_reserved', ['year' => date('Y'), 'app_name' => app_name()])
     </div>
 
     <div class="ml-auto">Theme by <a href="http://coreui.io">CoreUI</a></div>
