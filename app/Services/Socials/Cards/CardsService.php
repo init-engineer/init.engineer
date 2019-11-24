c66f3ffb3ddecf71a96641e1b564637b377c4b33
Kantai235 <kantai.developer@gmail.com>
2019-11-25 01:47:41 +0800

    Jobs 新增社群平台文章移除、Service 新增驅動 Jobs 執行移除社群文章的方法

--- app/Services/Socials/Cards/CardsService.php
+++ app/Services/Socials/Cards/CardsService.php
@@ -2,11 +2,16 @@
 
 namespace App\Services\Socials\Cards;
 
+use App\Models\Auth\User;
 use App\Models\Social\Cards;
 use App\Services\BaseService;
+use App\Jobs\Social\MediaCards\PlurkPrimaryDestory;
 use App\Jobs\Social\MediaCards\PlurkPrimaryPublish;
 use App\Jobs\Social\MediaCards\TwitterPrimaryPublish;
+use App\Jobs\Social\MediaCards\TwitterPrimaryDestory;
+use App\Jobs\Social\MediaCards\FacebookPrimaryDestory;
 use App\Jobs\Social\MediaCards\FacebookPrimaryPublish;
+use App\Jobs\Social\MediaCards\FacebookSecondaryDestory;
 use App\Jobs\Social\MediaCards\FacebookSecondaryPublish;
 
 /**
@@ -24,5 +29,19 @@ class CardsService extends BaseService implements CardsContract
         if (env('FACEBOOK_SECONDARY_CREATE_POST', false)) { FacebookSecondaryPublish::dispatch($cards); }
         if (env('TWITTER_CREATE_POST', false)) { TwitterPrimaryPublish::dispatch($cards); }
         if (env('PLURK_CREATE_POST', false)) { PlurkPrimaryPublish::dispatch($cards); }
+    }
+
+    /**
+     * @param User  $user
+     * @param Cards $cards
+     * @param array $options
+     * @return void
+     */
+    public function destory(User $user, Cards $cards, array $options)
+    {
+        FacebookPrimaryDestory::dispatch($user, $cards, $options);
+        FacebookSecondaryDestory::dispatch($user, $cards, $options);
+        TwitterPrimaryDestory::dispatch($user, $cards, $options);
+        PlurkPrimaryDestory::dispatch($user, $cards, $options);
     }
 }
