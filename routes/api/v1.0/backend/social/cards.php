f5f326e6e3689780e584e542431e2f84c4f419ae
Kantai235 <kantai.developer@gmail.com>
2019-11-27 10:47:57 +0800

    移除 routes 的 Api，重新命名為 api

---
+++ routes/api/v1.0/backend/social/cards.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\Api\Backend\Social\CardsController;
+
+/**
+ * All route names are prefixed with 'api.backend'.
+ */
+Route::group([
+    'prefix' => 'backend',
+    'as' => 'backend.',
+    'namespace' => 'Backend',
+], function () {
+    /**
+     * All route names are prefixed with 'api.backend.social'.
+     */
+    Route::group([
+        'prefix' => 'social',
+        'as' => 'social.',
+        'namespace' => 'Social',
+    ], function () {
+        /**
+         * All route names are prefixed with 'api.backend.social.cards'.
+         */
+        Route::group([
+            'prefix' => 'cards',
+            'as' => 'cards.',
+            'namespace' => 'Cards',
+        ], function () {
+            // Cards CRUD
+            Route::group([
+                'middleware' => [
+                    'auth:api',
+                    'role:'.config('access.users.admin_role'),
+                ],
+            ], function () {
+                Route::post('/', [CardsController::class, 'store'])->name('store');
+            });
+        });
+    });
+});
---
+++ routes/api/v1.0/frontend/social/cards.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\Api\Frontend\Social\CardsController;
+
+/**
+ * All route names are prefixed with 'api.frontend'.
+ */
+Route::group([
+    'prefix' => 'frontend',
+    'as' => 'frontend.',
+    'namespace' => 'Frontend',
+], function () {
+    /**
+     * All route names are prefixed with 'api.frontend.social'.
+     */
+    Route::group([
+        'prefix' => 'social',
+        'as' => 'social.',
+        'namespace' => 'Social',
+    ], function () {
+        /**
+         * All route names are prefixed with 'api.frontend.social.cards'.
+         */
+        Route::group([
+            'prefix' => 'cards',
+            'as' => 'cards.',
+            'namespace' => 'Cards',
+        ], function () {
+            // Cards CRUD
+            Route::get('/', [CardsController::class, 'index'])->name('index');
+            Route::group(['middleware' => 'auth:api'], function () {
+                Route::get('/dashboard', [CardsController::class, 'dashboard'])->name('dashboard');
+                Route::post('/', [CardsController::class, 'store'])->name('store');
+            });
+
+            // Specific Card
+            Route::group(['prefix' => '/{id}'], function () {
+                Route::get('/show', [CardsController::class, 'show'])->name('show');
+                Route::get('/links', [CardsController::class, 'links'])->name('links');
+                Route::get('/comments', [CardsController::class, 'comments'])->name('comments');
+            });
+        });
+    });
+});
