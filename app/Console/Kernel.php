38b0338158372d24f464abdbd132c7eeea30f1ae
Kantai235 <kantai.developer@gmail.com>
2019-11-30 03:23:45 +0800

    排程自動更新留言

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -42,10 +42,10 @@ class Kernel extends ConsoleKernel
         /**
          * 自動化 對社群平台爬蟲更新留言
          */
-        $schedule->command('social:media-cards-update all')->daily();
-        $schedule->command('social:media-cards-update 12')->hourly();
-        $schedule->command('social:media-cards-update 4')->everyFiveMinutes();
-        $schedule->command('social:media-cards-update 1')->everyMinute();
+        $schedule->command('social:comments-update all')->daily();
+        $schedule->command('social:comments-update 12')->hourly();
+        $schedule->command('social:comments-update 4')->everyFiveMinutes();
+        $schedule->command('social:comments-update 1')->everyMinute();
 
         /**
          * 自動化 執行任務、重新執行 Jobs 失敗的任務
