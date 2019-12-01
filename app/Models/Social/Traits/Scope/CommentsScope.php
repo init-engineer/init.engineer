935fe3407672e068f526d80d249d62f68cf20cc9
Kantai235 <kantai.developer@gmail.com>
2019-12-01 18:40:55 +0800

    Social Scope 新增 publish、banned

--- app/Models/Social/Traits/Scope/CommentsScope.php
+++ app/Models/Social/Traits/Scope/CommentsScope.php
@@ -17,4 +17,26 @@ trait CommentsScope
     {
         return $query->where('active', $status);
     }
+
+    /**
+     * @param $query
+     * @param bool $status
+     *
+     * @return mixed
+     */
+    public function scopePublish($query, $status = false)
+    {
+        return $query->where('is_banned', $status);
+    }
+
+    /**
+     * @param $query
+     * @param bool $status
+     *
+     * @return mixed
+     */
+    public function scopeBanned($query, $status = true)
+    {
+        return $query->where('is_banned', $status);
+    }
 }
