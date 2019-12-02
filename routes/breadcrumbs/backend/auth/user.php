6787295e67363fad32dc01e805c975be2afd3c4d
Kantai235 <kantai.developer@gmail.com>
2019-12-02 10:15:26 +0800

    後台 Auth Search 的麵包屑路徑

--- routes/breadcrumbs/backend/auth/user.php
+++ routes/breadcrumbs/backend/auth/user.php
@@ -4,6 +4,11 @@ Breadcrumbs::for('admin.auth.user.index', function ($trail) {
     $trail->push(__('labels.backend.access.users.management'), route('admin.auth.user.index'));
 });
 
+Breadcrumbs::for('admin.auth.user.search', function ($trail) {
+    $trail->parent('admin.dashboard');
+    $trail->push(__('labels.backend.access.users.management'), route('admin.auth.user.search'));
+});
+
 Breadcrumbs::for('admin.auth.user.deactivated', function ($trail) {
     $trail->parent('admin.auth.user.index');
     $trail->push(__('menus.backend.access.users.deactivated'), route('admin.auth.user.deactivated'));
