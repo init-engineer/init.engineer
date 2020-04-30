bb4267353ad39176bfe7907934b3a8c7e2df7d40
Kantai <kantai.developer@gmail.com>
2020-05-01 02:01:08 +0800

    首頁移除 Neo Banner，新增 Wowa Banner

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,6 +3,20 @@
 @section('title', app_name() . ' | ' . __('navs.general.home'))
 
 @section('content')
+    <div class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <iframe src="https://aidn.jp/wowa/94697596" class="pt-1 w-100" style="height: 60vh;" frameborder="0"></iframe>
+                <div class="carousel-caption text-left mb-5 pb-5 text-dark">
+                    <h1 class="text-black-stroke display-3">純靠北工程師</h1>
+                    <p class="lead">發源自臉書──全台最大工程師廢文社群</p>
+                    <a class="btn btn-warning btn-lg shake-slow shake-constant shake-constant--hover" href="{{ route('frontend.social.cards.create') }}" role="button">開始去發廢文 <i class="fas fa-poop"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--
     <div class="carousel slide d-none d-md-block" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -31,6 +45,7 @@
             </div>
         </div>
     </div>
+    -->
 
     <div class="container-fluid mb-5 animated fadeIn">
         <div class="row my-4">
