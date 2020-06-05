8310a9667ac622d3f6d9d968dfee16202909afd0
Kantai <kantai.developer@gmail.com>
2020-06-05 11:37:34 +0800

    群眾審核增加 Expert 等級，並將身份組距離折疊

--- resources/js/frontend/components/social/cards/SocialCardsPoint.vue
+++ resources/js/frontend/components/social/cards/SocialCardsPoint.vue
@@ -2,60 +2,80 @@
     <div class="jumbotron py-4">
         <h3>
             您代表了 {{ point }} 張票，好棒！
-            <button class="btn btn-info" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">展開詳細規則</button>
+            <button class="btn btn-info" type="button" data-toggle="collapse" data-target="#展開詳細規則" aria-expanded="false" aria-controls="展開詳細規則">展開詳細規則</button>
+            <button class="btn btn-info" type="button" data-toggle="collapse" data-target="#展開你與身份組的距離" aria-expanded="false" aria-controls="展開你與身份組的距離">展開你與身份組的距離</button>
         </h3>
-        <hr>
-        <h3>你與下個身份組的距離：</h3>
-        <div class="flex-wrapper">
-            <div class="single-chart" v-if="percentage.junior != null">
-                <svg viewBox="0 0 36 36" class="circular-chart orange">
-                    <path class="circle-bg"
-                        d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"/>
-                    <path class="circle"
-                        :stroke-dasharray="`${percentage.junior}, 100`"
-                        d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"/>
-                    <text x="18" y="18.35" class="percentage">{{ percentage.junior }}%</text>
-                    <text x="18" y="24.35" class="percentage-text">Junior</text>
-                </svg>
-            </div>
+        <div class="collapse" id="展開你與身份組的距離">
+            <hr>
+            <h3>你與下個身份組的距離：</h3>
+            <div class="flex-wrapper">
+                <div class="single-chart" v-if="percentage.junior != null">
+                    <svg viewBox="0 0 36 36" class="circular-chart red">
+                        <path class="circle-bg"
+                            d="M18 2.0845
+                            a 15.9155 15.9155 0 0 1 0 31.831
+                            a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                        <path class="circle"
+                            :stroke-dasharray="`${percentage.junior}, 100`"
+                            d="M18 2.0845
+                            a 15.9155 15.9155 0 0 1 0 31.831
+                            a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                        <text x="18" y="18.35" class="percentage">{{ percentage.junior }}%</text>
+                        <text x="18" y="24.35" class="percentage-text">Junior</text>
+                    </svg>
+                </div>
 
-            <div class="single-chart" v-if="percentage.senior != null">
-                <svg viewBox="0 0 36 36" class="circular-chart green">
-                    <path class="circle-bg"
-                        d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"/>
-                    <path class="circle"
-                        :stroke-dasharray="`${percentage.senior}, 100`"
-                        d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"/>
-                    <text x="18" y="18.35" class="percentage">{{ percentage.senior }}%</text>
-                    <text x="18" y="24.35" class="percentage-text">Senior</text>
-                </svg>
-            </div>
+                <div class="single-chart" v-if="percentage.senior != null">
+                    <svg viewBox="0 0 36 36" class="circular-chart orange">
+                        <path class="circle-bg"
+                            d="M18 2.0845
+                            a 15.9155 15.9155 0 0 1 0 31.831
+                            a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                        <path class="circle"
+                            :stroke-dasharray="`${percentage.senior}, 100`"
+                            d="M18 2.0845
+                            a 15.9155 15.9155 0 0 1 0 31.831
+                            a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                        <text x="18" y="18.35" class="percentage">{{ percentage.senior }}%</text>
+                        <text x="18" y="24.35" class="percentage-text">Senior</text>
+                    </svg>
+                </div>
 
-            <div class="single-chart" v-if="percentage.legend != null">
-                <svg viewBox="0 0 36 36" class="circular-chart blue">
-                    <path class="circle-bg"
-                        d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"/>
-                    <path class="circle"
-                        :stroke-dasharray="`${percentage.legend}, 100`"
-                        d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"/>
-                    <text x="18" y="18.35" class="percentage">{{ percentage.legend }}%</text>
-                    <text x="18" y="24.35" class="percentage-text">Legend</text>
-                </svg>
+                <div class="single-chart" v-if="percentage.expert != null">
+                    <svg viewBox="0 0 36 36" class="circular-chart yellow">
+                        <path class="circle-bg"
+                            d="M18 2.0845
+                            a 15.9155 15.9155 0 0 1 0 31.831
+                            a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                        <path class="circle"
+                            :stroke-dasharray="`${percentage.expert}, 100`"
+                            d="M18 2.0845
+                            a 15.9155 15.9155 0 0 1 0 31.831
+                            a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                        <text x="18" y="18.35" class="percentage">{{ percentage.expert }}%</text>
+                        <text x="18" y="24.35" class="percentage-text">Expert</text>
+                    </svg>
+                </div>
+
+                <div class="single-chart" v-if="percentage.legend != null">
+                    <svg viewBox="0 0 36 36" class="circular-chart green">
+                        <path class="circle-bg"
+                            d="M18 2.0845
+                            a 15.9155 15.9155 0 0 1 0 31.831
+                            a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                        <path class="circle"
+                            :stroke-dasharray="`${percentage.legend}, 100`"
+                            d="M18 2.0845
+                            a 15.9155 15.9155 0 0 1 0 31.831
+                            a 15.9155 15.9155 0 0 1 0 -31.831"/>
+                        <text x="18" y="18.35" class="percentage">{{ percentage.legend }}%</text>
+                        <text x="18" y="24.35" class="percentage-text">Legend</text>
+                    </svg>
+                </div>
             </div>
         </div>
-        <div class="collapse" id="collapseExample">
+        <div class="collapse" id="展開詳細規則">
+            <hr>
             <div class="card card-body">
                 <p class="lead">文章審核通過規則:</p>
                 <ul>
@@ -93,6 +113,7 @@ export default {
             percentage: {
                 junior: null,
                 senior: null,
+                expert: null,
                 legend: null,
             },
         };
@@ -109,12 +130,20 @@ export default {
                         if (element == "administrator") this.roles.push({name: element, point: 99});
                         if (element == "junior vip") this.roles.push({name: element, point: 1});
                         if (element == "senior vip") this.roles.push({name: element, point: 2});
-                        if (element == "junior donate") this.roles.push({name: element, point: 2});
-                        if (element == "senior donate") this.roles.push({name: element, point: 4});
+                        if (element == "expert vip") this.roles.push({name: element, point: 3});
+                        if (element == "legend vip") this.roles.push({name: element, point: 4});
+                        if (element == "junior donate") this.roles.push({name: element, point: 1});
+                        if (element == "senior donate") this.roles.push({name: element, point: 2});
+                        if (element == "expert donate") this.roles.push({name: element, point: 3});
+                        if (element == "legend donate") this.roles.push({name: element, point: 4});
                         if (element == "junior user") this.roles.push({name: element, point: 1});
-                        if (element == "senior user") this.roles.push({name: element, point: 2});
-                        if (element == "junior manager") this.roles.push({name: element, point: 4});
-                        if (element == "senior manager") this.roles.push({name: element, point: 9});
+                        if (element == "senior user") this.roles.push({name: element, point: 1});
+                        if (element == "expert user") this.roles.push({name: element, point: 2});
+                        if (element == "legend user") this.roles.push({name: element, point: 3});
+                        if (element == "junior manager") this.roles.push({name: element, point: 2});
+                        if (element == "senior manager") this.roles.push({name: element, point: 2});
+                        if (element == "expert manager") this.roles.push({name: element, point: 3});
+                        if (element == "legend manager") this.roles.push({name: element, point: 4});
                         if (element == "user") this.roles.push({name: element, point: 1});
                     });
 
@@ -129,7 +158,8 @@ export default {
 
                     this.percentage.junior = Math.round((this.count * 100) / 200);
                     this.percentage.senior = Math.round((this.count * 100) / 500);
-                    this.percentage.legend = Math.round((this.count * 100) / 1000);
+                    this.percentage.expert = Math.round((this.count * 100) / 1000);
+                    this.percentage.legend = Math.round((this.count * 100) / 3000);
                 })
                 .catch(error => console.log(error));
         },
@@ -145,64 +175,68 @@ export default {
 
 <style scoped>
 .flex-wrapper {
-  display: flex;
-  flex-flow: row nowrap;
+    display: flex;
+    flex-flow: row nowrap;
 }
 
 .single-chart {
-  width: 33%;
-  justify-content: space-around ;
+    width: 33%;
+    justify-content: space-around ;
 }
 
 .circular-chart {
-  display: block;
-  margin: 10px auto;
-  max-width: 80%;
-  max-height: 250px;
+    display: block;
+    margin: 10px auto;
+    max-width: 80%;
+    max-height: 250px;
 }
 
 .circle-bg {
-  fill: none;
-  stroke: #cacaca;
-  stroke-width: 3.8;
+    fill: none;
+    stroke: #cacaca;
+    stroke-width: 3.8;
 }
 
 .circle {
-  fill: none;
-  stroke-width: 2.8;
-  stroke-linecap: round;
-  animation: progress 1s ease-out forwards;
+    fill: none;
+    stroke-width: 2.8;
+    stroke-linecap: round;
+    animation: progress 1s ease-out forwards;
 }
 
 @keyframes progress {
-  0% {
-    stroke-dasharray: 0 100;
-  }
+    0% {
+        stroke-dasharray: 0 100;
+    }
+}
+
+.circular-chart.red .circle {
+    stroke: #e76f51;
 }
 
 .circular-chart.orange .circle {
-  stroke: #ff9f00;
+    stroke: #f4a261;
+}
+
+.circular-chart.yellow .circle {
+    stroke: #e9c46a;
 }
 
 .circular-chart.green .circle {
-  stroke: #4CC790;
-}
-
-.circular-chart.blue .circle {
-  stroke: #3c9ee5;
+    stroke: #2a9d8f;
 }
 
 .percentage {
-  fill: #666;
-  font-family: sans-serif;
-  font-size: 0.5em;
-  text-anchor: middle;
+    fill: #666;
+    font-family: sans-serif;
+    font-size: 0.5em;
+    text-anchor: middle;
 }
 
 .percentage-text {
-  fill: #666;
-  font-family: sans-serif;
-  font-size: 0.3em;
-  text-anchor: middle;
+    fill: #666;
+    font-family: sans-serif;
+    font-size: 0.3em;
+    text-anchor: middle;
 }
 </style>
