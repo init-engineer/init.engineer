640d7329e7de81bb0b92e6eedc82d8869053d268
Kantai <kantai.developer@gmail.com>
2020-07-03 14:02:02 +0800

    首頁換成ケモノ着ぐるみ

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,6 +3,23 @@
 @section('title', app_name() . ' | ' . __('navs.general.home'))
 
 @section('content')
+    <!-- ケモノ着ぐるみ -->
+    <div class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://i.ytimg.com/vi/BrCwa3O8fdo/maxresdefault.jpg" class="d-block w-100" style="height: 60vh; object-fit: cover;" alt="ケモノ着ぐるみ">
+                <div class="carousel-caption text-left mb-5 pb-5 text-dark">
+                    <h1 class="text-black-stroke display-3">純靠北工程師</h1>
+                    <p class="lead">發源自臉書──全台最大工程師廢文社群</p>
+                    <a class="btn btn-warning btn-lg shake-slow shake-constant shake-constant--hover" href="{{ route('frontend.social.cards.create') }}" role="button">開始去發廢文 <i class="fas fa-poop"></i></a>
+                </div>
+                <iframe class="mt-2" width="0" height="0" src="https://www.youtube.com/embed/BrCwa3O8fdo?rel=0&autoplay=1&loop=1&playlist=BrCwa3O8fdo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+
+    <!-- Wonder of Wonder Art -->
+    <!--
     <div class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -15,7 +32,9 @@
             </div>
         </div>
     </div>
+    -->
 
+    <!-- 駭客任務 Neo -->
     <!--
     <div class="carousel slide d-none d-md-block" data-ride="carousel">
         <div class="carousel-inner">
