d1a89a2bef6831dbd97710b769b89407e8f64c1b
Kantai <kantai.developer@gmail.com>
2020-09-09 11:49:58 +0800

    修正廣告文章測試 command

--- app/Console/Commands/Testing/SocialCardsAds.php
+++ app/Console/Commands/Testing/SocialCardsAds.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands\Testing;
 
+use App\Models\Social\Cards;
 use Illuminate\Console\Command;
 use App\Repositories\Frontend\Social\AdsRepository;
 use App\Services\Socials\Images\ImagesService;
@@ -55,14 +56,16 @@ class SocialCardsAds extends Command
      */
     public function handle()
     {
-        $ads = $this->adsRepository->findRandom();
-        $adsImage = imageCreateFromPng(asset($ads->ads_path));
+        $card = Cards::find(1);
+        $ads = $this->adsRepository->findRandom($card);
 
-        // $image = $this->imagesService->buildImage([
-        //     'content' => '測試廣告效果',
-        //     'themeStyle' => 'j874kwoxi2nh64yt67wtphy9m5dmea4q',
-        //     'fontStyle' => 'ea98dde8987df3cd8aef75479019b688',
-        // ]);
+        $image = $this->imagesService->buildImage([
+            'content' => '測試廣告效果',
+            'themeStyle' => '2e6046c7387d8fbe9acd700394a3add3',
+            // 'themeStyle' => '32d2a897602ef652ed8e15d66128aa74',
+            // 'themeStyle' => 'a5c95b86291ea299fcbe64458ed12702',
+            'fontStyle' => 'ea98dde8987df3cd8aef75479019b688',
+        ], $card);
 
         // dd($image);
     }
