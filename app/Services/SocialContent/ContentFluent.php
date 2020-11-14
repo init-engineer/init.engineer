99eaf6d250b3ea0652c20c9f8a88e95695b05d7c
乾太 <kantai.developer@gmail.com>
2020-11-14 13:29:53 +0800

    修改 ContentFluent 發文內容格式

--- app/Services/SocialContent/ContentFluent.php
+++ app/Services/SocialContent/ContentFluent.php
@@ -71,13 +71,15 @@ class ContentFluent
     public function get(): string
     {
         $response = "#" . app_name() . base_convert($this->id, 10, 36) . "\n\r----------\n\r";
-        $content = ($this->limit != 0 && mb_strlen($this->content, 'utf-8') > $this->limit) ? mb_substr($this->content, 0, $this->limit, 'utf-8') . ' ...' : $this->content;
-        $response = $response . $content;
 
         if (isset($this->footerOption['review']) && $this->footerOption['review']) $response = $response . "🗳️ [群眾審核] " . route('frontend.social.cards.review') . "\n\r";
         if (isset($this->footerOption['github']) && $this->footerOption['github']) $response = $response . "👉 [GitHub Repo] https://github.com/init-engineer/init.engineer" . "\n\r";
         if (isset($this->footerOption['publish']) && $this->footerOption['publish']) $response = $response . "📢 [匿名發文] " . route('frontend.social.cards.create') . "\n\r";
         if (isset($this->footerOption['show']) && $this->footerOption['show']) $response = $response . "🥙 [全平台留言] " . route('frontend.social.cards.show', ['id' => $this->id]) . "\n\r";
+
+        $response = $response . "\n\r----------\n\r";
+        $content = ($this->limit != 0 && mb_strlen($this->content, 'utf-8') > $this->limit) ? mb_substr($this->content, 0, $this->limit, 'utf-8') . ' ...' : $this->content;
+        $response = $response . $content;
 
         return $response;
     }
