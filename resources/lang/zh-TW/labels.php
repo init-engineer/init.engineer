6a744307c28493f5fbbd85d84d67cecf338d7ec1
乾太 <kantai.developer@gmail.com>
2020-11-14 14:05:26 +0800

    修正文字語系檔案

--- resources/lang/zh-TW/labels.php
+++ resources/lang/zh-TW/labels.php
@@ -187,6 +187,10 @@ return [
         ],
 
         'user' => [
+            'dashboard' => [
+                'dashboard_text' => 'DASHBOARD',
+            ],
+
             'passwords' => [
                 'change' => '更改密碼',
             ],
