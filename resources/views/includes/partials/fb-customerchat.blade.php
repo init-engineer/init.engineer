bc82f28b7c59455eabdf0c596ccf3f303b9d703a
乾太 <kantai.developer@gmail.com>
2020-11-17 16:33:33 +0800

    Update fb-customerchat.blade.php

--- resources/views/includes/partials/fb-customerchat.blade.php
+++ resources/views/includes/partials/fb-customerchat.blade.php
@@ -20,7 +20,30 @@
     </script>
 
     <!-- Your customer chat code -->
-    <div class="fb-customerchat" attribution=setup_tool page_id="{{ config('facebook.customerchat') }}" theme_color="#13cf13"
-        logged_in_greeting="蛤？" logged_out_greeting="蛤？">
+    <div class="fb-customerchat"
+        attribution="setup_tool"
+        page_id="{{ config('facebook.customerchat') }}"
+        theme_color="#13cf13"
+        greeting_dialog_display="hide"
+        logged_in_greeting="哈囉，因為版主不在家，所以這邊整理一些常見問題來給你們自己找答案，如果有其他問題，您可以靜待版主回來再回覆您。
+
+        投稿、發表文章
+        https://kaobei.engineer/cards/create
+
+        參與群眾審核
+        https://kaobei.engineer/cards/review
+
+        參與專案開發
+        https://github.com/init-engineer/init.engineer"
+        logged_out_greeting="哈囉，因為版主不在家，所以這邊整理一些常見問題來給你們自己找答案，如果有其他問題，您可以靜待版主回來再回覆您。
+
+        投稿、發表文章
+        https://kaobei.engineer/cards/create
+
+        參與群眾審核
+        https://kaobei.engineer/cards/review
+
+        參與專案開發
+        https://github.com/init-engineer/init.engineer">
     </div>
 @endif
