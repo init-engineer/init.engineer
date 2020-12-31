3db3181d267002ac99836256158c2206eec81ad9
乾太 <kantai.developer@gmail.com>
2021-01-01 05:30:54 +0800

    移除聯絡我們相關功能

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -13,8 +13,6 @@ use App\Http\Controllers\Frontend\OAuth\AuthorizeController;
  */
 Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/policies', [HomeController::class, 'policies'])->name('policies');
-Route::get('/contact', [ContactController::class, 'index'])->name('contact');
-Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.send');
 
 /**
  * These frontend controllers require the user to be logged in
