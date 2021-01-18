1ad2a7469357b4fa5508ddf29ae4e8b084e6bda3
乾太 <kantai.developer@gmail.com>
2021-01-18 23:16:27 +0800

    修正 AdBlock 有關閉依然顯示的問題

--- resources/js/frontend/components/home/AdBlockWarning.vue
+++ resources/js/frontend/components/home/AdBlockWarning.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <div v-if="block" class="alert alert-danger alert-dismissible fade show" role="alert">
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
