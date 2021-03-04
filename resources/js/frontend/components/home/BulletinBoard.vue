0983ff2f8b01aa48e730b9a9807e203abfe25b77
乾太 <kantai.developer@gmail.com>
2021-03-05 04:16:54 +0800

    更新公告欄內容

--- resources/js/frontend/components/home/BulletinBoard.vue
+++ resources/js/frontend/components/home/BulletinBoard.vue
@@ -19,6 +19,26 @@ export default {
     return {
         bulletins: [
             {
+                date: "2021/03/05 (週五)",
+                title: "更新 footer",
+                content: "頁尾更新圖示，外部連結以開啟新分頁的方式執行。",
+            },
+            {
+                date: "2021/02/15 (週一)",
+                title: "註冊新使用者的信箱限制",
+                content: "重新開啟信箱註冊使用者的功能，但僅支援 @gmail.com、@yahoo.com、@icloud.com、@me.com、@edu、@hotmail.com 以及 @outlook.com 上述這些信箱可申請。",
+            },
+            {
+                date: "2021/02/01 (週一)",
+                title: "修正發表文章樣式",
+                content: "修正支離破碎主題的 Bug，以及 To Be Continued 的位置錯置問題。",
+            },
+            {
+                date: "2021/01/30 (週六)",
+                title: "新增社群平台支援",
+                content: "支援 Telegram、Tumblr 兩個社群平台。",
+            },
+            {
                 date: "2021/01/18 (週一)",
                 title: "更新廣告",
                 content: "1. 移除群眾審核系統的史蒂夫和戴夫廣告。\n\r2. 移除穿插(換頁滿版)廣告。\n\r3. AdSense 廣告改為插入卡片的方式。\n\r4. 新增很派的 AdBlock 阻擋提醒。",
