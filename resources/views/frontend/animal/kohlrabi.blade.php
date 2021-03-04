c29d47e56ff2715833c6a60c190073a70fae715f
乾太 <kantai.developer@gmail.com>
2021-03-05 04:41:34 +0800

    修正大頭菜計算機的 Banner 圖片命名

--- resources/views/frontend/animal/kohlrabi.blade.php
+++ resources/views/frontend/animal/kohlrabi.blade.php
@@ -4,14 +4,14 @@
 @section('meta_keyword', app_name() . ' | 動物森友會 - 大頭菜計算機')
 @section('meta_description', app_name() . ' | 動物森友會 - 大頭菜計算機，計算你的大頭菜')
 @section('meta_og_title', app_name() . ' | 動物森友會 - 大頭菜計算機')
-@section('meta_og_image', asset('img/frontend/banner/KohlrabiComputer.png'))
+@section('meta_og_image', asset('img/frontend/banner/turnips-computer.png'))
 @section('meta_og_description', app_name() . ' | 動物森友會 - 大頭菜計算機，計算你的大頭菜')
 
 @section('content')
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col col-12 col-md-6">
-                <img class="img-fluid rounded mb-5 w-100" src="{{ asset('img/frontend/banner/KohlrabiComputer.png') }}" />
+                <img class="img-fluid rounded mb-5 w-100" src="{{ asset('img/frontend/banner/turnips-computer.png') }}" />
             </div>
             <!--col-->
             <div class="col-12">
