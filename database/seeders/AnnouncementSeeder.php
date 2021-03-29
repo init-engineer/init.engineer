bf8fd1547b23117359b0a621f6b8c78ff8c77bdd
Kantai <kantai.developer@gmail.com>
2021-03-29 16:32:16 +0800

    Update AnnouncementSeeder.php

--- database/seeders/AnnouncementSeeder.php
+++ database/seeders/AnnouncementSeeder.php
@@ -27,59 +27,59 @@ class AnnouncementSeeder extends Seeder
             /*
              * Note: There is currently no UI for this feature. If you are going to build a UI, and if you are going to use a WYSIWYG editor for the message (because it supports HTML on the frontend) that you properly sanitize the input before it is stored in the database.
              */
-//            Announcement::create([
-//                'area' => null,
-//                'type' => 'info',
-//                'message' => 'This is a <strong>Global</strong> announcement that will show on both the frontend and backend. <em>See <strong>AnnouncementSeeder</strong> for more usage examples.</em>',
-//                'enabled' => true,
-//            ]);
-//
-//            Announcement::create([
-//                'area' => 'frontend',
-//                'type' => 'warning',
-//                'message' => 'This is a <strong>Frontend</strong> announcement that will not show on the backend.',
-//                'enabled' => true,
-//            ]);
-//
-//            Announcement::create([
-//                'area' => 'backend',
-//                'type' => 'danger',
-//                'message' => 'This is a <strong>Backend</strong> announcement that will not show on the frontend.',
-//                'enabled' => true,
-//            ]);
-//
-//            Announcement::create([
-//                'area' => null,
-//                'type' => 'danger',
-//                'message' => 'This announcement will be shown because the current time falls between the start and end dates.' ,
-//                'enabled' => true,
-//                'starts_at' => now()->subWeek(),
-//                'ends_at' => now()->addWeek()
-//            ]);
-//
-//            Announcement::create([
-//                'area' => null,
-//                'type' => 'danger',
-//                'message' => 'This announcement will not be shown because it is disabled.' ,
-//                'enabled' => false,
-//            ]);
-//
-//            Announcement::create([
-//                'area' => null,
-//                'type' => 'danger',
-//                'message' => 'This announcement will not be shown because the end date has passed.' ,
-//                'enabled' => true,
-//                'ends_at' => now()->subDay()
-//            ]);
-//
-//            Announcement::create([
-//                'area' => null,
-//                'type' => 'danger',
-//                'message' => 'This announcement will not be shown because the current time does not fall between the start and end dates.' ,
-//                'enabled' => true,
-//                'starts_at' => now()->subWeek(),
-//                'ends_at' => now()->subDay()
-//            ]);
+            // Announcement::create([
+            //     'area' => null,
+            //     'type' => 'info',
+            //     'message' => '這是一個<strong>全域</strong>公告，將同時顯示在前臺和後臺，<em>有關更多用法範例，請參見 <strong>AnnouncementSeeder</strong>。</em>',
+            //     'enabled' => true,
+            // ]);
+
+            // Announcement::create([
+            //     'area' => 'frontend',
+            //     'type' => 'warning',
+            //     'message' => '這是一個<strong>前臺</strong>公告，不會在後臺顯示。',
+            //     'enabled' => true,
+            // ]);
+
+            // Announcement::create([
+            //     'area' => 'backend',
+            //     'type' => 'danger',
+            //     'message' => '這是一個<strong>後臺</strong>公告，不會在前臺顯示。',
+            //     'enabled' => true,
+            // ]);
+
+            // Announcement::create([
+            //     'area' => null,
+            //     'type' => 'danger',
+            //     'message' => '這則公告僅會顯示在限定時間內。',
+            //     'enabled' => true,
+            //     'starts_at' => now()->subWeek(),
+            //     'ends_at' => now()->addWeek()
+            // ]);
+
+            // Announcement::create([
+            //     'area' => null,
+            //     'type' => 'danger',
+            //     'message' => '這則公告已被停用。',
+            //     'enabled' => false,
+            // ]);
+
+            // Announcement::create([
+            //     'area' => null,
+            //     'type' => 'danger',
+            //     'message' => '這則公告已過期，因此不會顯示。',
+            //     'enabled' => true,
+            //     'ends_at' => now()->subDay()
+            // ]);
+
+            // Announcement::create([
+            //     'area' => null,
+            //     'type' => 'danger',
+            //     'message' => '這則公告尚未開始，因此不會顯示。',
+            //     'enabled' => true,
+            //     'starts_at' => now()->subWeek(),
+            //     'ends_at' => now()->subDay()
+            // ]);
         }
 
         $this->enableForeignKeys();
