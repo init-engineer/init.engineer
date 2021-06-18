be707b85ee13b17b5124682585168729ea058043
乾太 <kantai.developer@gmail.com>
2021-06-18 09:16:31 +0800

    Update Announcement.php

--- app/Domains/Announcement/Models/Announcement.php
+++ app/Domains/Announcement/Models/Announcement.php
@@ -11,34 +11,6 @@ use Spatie\Activitylog\Traits\LogsActivity;
 
 /**
  * Class Announcement.
- *
- * @property int $id
- * @property string|null $area
- * @property string $type
- * @property string $message
- * @property bool $enabled
- * @property \Illuminate\Support\Carbon|null $starts_at
- * @property \Illuminate\Support\Carbon|null $ends_at
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\Activitylog\Models\Activity[] $activities
- * @property-read int|null $activities_count
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement enabled()
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement forArea($area)
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement inTimeFrame()
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement query()
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement whereArea($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement whereEnabled($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement whereEndsAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement whereMessage($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement whereStartsAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement whereType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Announcement whereUpdatedAt($value)
- * @mixin \Eloquent
  */
 class Announcement extends Model
 {
