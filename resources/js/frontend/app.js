99c1b14a148dc2b910283d2b2277afe1ccd5e115
乾太 <kantai.developer@gmail.com>
2021-06-22 01:56:21 +0800

    Update app.js

--- resources/js/frontend/app.js
+++ resources/js/frontend/app.js
@@ -20,6 +20,8 @@ import Vue from 'vue';
 const files = require.context('./', true, /\.vue$/i)
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
