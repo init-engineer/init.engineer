a85b2b5a41d429f27466953d8365be726ed506d8
乾太 <kantai.developer@gmail.com>
2021-06-22 02:02:46 +0800

    User and Role 將 Livewire 抽離出來 row.blade.php

--- app/Http/Livewire/Backend/UsersTable.php
+++ app/Http/Livewire/Backend/UsersTable.php
@@ -4,44 +4,38 @@ namespace App\Http\Livewire\Backend;
 
 use App\Domains\Auth\Models\User;
 use Illuminate\Database\Eloquent\Builder;
-use Rappasoft\LaravelLivewireTables\TableComponent;
-use Rappasoft\LaravelLivewireTables\Traits\HtmlComponents;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\Views\Filter;
 
 /**
  * Class UsersTable.
  */
-class UsersTable extends TableComponent
+class UsersTable extends DataTableComponent
 {
-    use HtmlComponents;
-
     /**
-     * @var string
-     *
-     * The initial field to be sorting by.
-     */
-    public $sortField = 'name';
-
-    /**
-     * @var string
+     * @var
      */
     public $status;
 
     /**
-     * @var array
+     * @var array|string[]
      */
-    protected $options = [
-        // The class set on the table when using bootstrap
-        'bootstrap.classes.table' => 'table table-striped',
-
-        // Whether or not the table is wrapped in a `.container-fluid` or not
-        'bootstrap.container' => false,
+    public array $sortNames = [
+        'email_verified_at' => 'Verified',
+        'two_factor_auth_count' => '2FA',
     ];
 
     /**
-     * @param string $status
-     *
-     * @return void
+     * @var array|string[]
+     */
+    public array $filterNames = [
+        'type' => 'User Type',
+        'verified' => 'E-mail Verified',
+    ];
+
+    /**
+     * @param  string  $status
      */
     public function mount($status = 'active'): void
     {
@@ -53,18 +47,48 @@ class UsersTable extends TableComponent
      */
     public function query(): Builder
     {
-        $query = User::with('roles', 'twoFactorAuth')
-            ->withCount('twoFactorAuth');
+        $query = User::with('roles', 'twoFactorAuth')->withCount('twoFactorAuth');
 
         if ($this->status === 'deleted') {
-            return $query->onlyTrashed();
+            $query = $query->onlyTrashed();
+        } elseif ($this->status === 'deactivated') {
+            $query = $query->onlyDeactivated();
+        } else {
+            $query = $query->onlyActive();
         }
 
-        if ($this->status === 'deactivated') {
-            return $query->onlyDeactivated();
-        }
+        return $query
+            ->when($this->getFilter('search'), fn ($query, $term) => $query->search($term))
+            ->when($this->getFilter('type'), fn ($query, $type) => $query->where('type', $type))
+            ->when($this->getFilter('active'), fn ($query, $active) => $query->where('active', $active === 'yes'))
+            ->when($this->getFilter('verified'), fn ($query, $verified) => $verified === 'yes' ? $query->whereNotNull('email_verified_at') : $query->whereNull('email_verified_at'));
+    }
 
-        return $query->onlyActive();
+    /**
+     * @return array
+     */
+    public function filters(): array
+    {
+        return [
+            'type' => Filter::make('User Type')
+                ->select([
+                    '' => 'Any',
+                    User::TYPE_ADMIN => 'Administrators',
+                    User::TYPE_USER => 'Users',
+                ]),
+            'active' => Filter::make('Active')
+                ->select([
+                    '' => 'Any',
+                    'yes' => 'Yes',
+                    'no' => 'No',
+                ]),
+            'verified' => Filter::make('E-mail Verified')
+                ->select([
+                    '' => 'Any',
+                    'yes' => 'Yes',
+                    'no' => 'No',
+                ]),
+        ];
     }
 
     /**
@@ -73,54 +97,27 @@ class UsersTable extends TableComponent
     public function columns(): array
     {
         return [
-            Column::make(__('Type'), 'type')
-                ->sortable()
-                ->format(function (User $model) {
-                    return view('backend.auth.user.includes.type', ['user' => $model]);
-                }),
-            Column::make(__('Name'), 'name')
-                ->searchable()
+            Column::make(__('Type'))
+                ->sortable(),
+            Column::make(__('Name'))
                 ->sortable(),
             Column::make(__('E-mail'), 'email')
-                ->searchable()
-                ->sortable()
-                ->format(function (User $model) {
-                    return $this->mailto($model->email);
-                }),
+                ->sortable(),
             Column::make(__('Verified'), 'email_verified_at')
-                ->sortable()
-                ->format(function (User $model) {
-                    return view('backend.auth.user.includes.verified', ['user' => $model]);
-                }),
-            Column::make(__('2FA'))
-                ->sortable(function ($builder, $direction) {
-                    return $builder->orderBy('two_factor_auth_count', $direction);
-                })
-                ->format(function (User $model) {
-                    return view('backend.auth.user.includes.2fa', ['user' => $model]);
-                }),
-            Column::make(__('Roles'), 'roles_label')
-                ->searchable(function ($builder, $term) {
-                    return $builder->orWhereHas('roles', function ($query) use ($term) {
-                        return $query->where('name', 'like', '%' . $term . '%');
-                    });
-                })
-                ->format(function (User $model) {
-                    return $this->html($model->roles_label);
-                }),
-            Column::make(__('Additional Permissions'), 'permissions_label')
-                ->searchable(function ($builder, $term) {
-                    return $builder->orWhereHas('permissions', function ($query) use ($term) {
-                        return $query->where('name', 'like', '%' . $term . '%');
-                    });
-                })
-                ->format(function (User $model) {
-                    return $this->html($model->permissions_label);
-                }),
-            Column::make(__('Actions'))
-                ->format(function (User $model) {
-                    return view('backend.auth.user.includes.actions', ['user' => $model]);
-                }),
+                ->sortable(),
+            Column::make(__('2FA'), 'two_factor_auth_count')
+                ->sortable(),
+            Column::make(__('Roles')),
+            Column::make(__('Additional Permissions')),
+            Column::make(__('Actions')),
         ];
+    }
+
+    /**
+     * @return string
+     */
+    public function rowView(): string
+    {
+        return 'backend.auth.user.includes.row';
     }
 }
