f87035f0d20abe7f6a8c099844347fe815759f43
Kantai <kantai.developer@gmail.com>
2021-08-26 22:32:12 +0800

    修改文章列表 card-header 的內容

--- resources/views/frontend/social/index.blade.php
+++ resources/views/frontend/social/index.blade.php
@@ -2,13 +2,24 @@
 
 @section('title', __('Init.Engineer Submit'))
 
+@push('after-styles')
+    <style>
+        .card-header {
+            padding: 2px !important;
+            margin: 0px !important;
+        }
+    </style>
+@endpush
+
 @section('content')
-    <div class="container py-4">
+    <div class="container-fluid py-4">
         <div class="row justify-content-center">
             <div class="col-md-9 order-md-first order-last">
                 <x-frontend.card>
                     <x-slot name="header">
-                        @lang('Init.Engineer Submit')
+                        <marquee style="height: 32px;">
+                            <p style="font-size: 24px;">@lang('Advertising content 1')</p>
+                        </marquee>
                     </x-slot>
 
                     <x-slot name="body">
