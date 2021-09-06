e4dd5a039dae790d476ff97a65cf86816f460692
乾太 <kantai.developer@gmail.com>
2021-09-06 23:51:00 +0800

    微調發表文章的版規區塊，避免 RWD 跑版

--- resources/js/frontend/components/social/PublishArticle.vue
+++ resources/js/frontend/components/social/PublishArticle.vue
@@ -28,7 +28,7 @@
                                         required
                                         @keyup="onKeyupListener($event)"></textarea>
                                 </div>
-                                <div class="buttons">
+                                <div class="buttons pt-2">
                                     <button class="next" disabled>下一步</button>
                                 </div>
                             </div>
@@ -58,7 +58,7 @@
                                 </div>
                             </div>
                             <div class="content">
-                                <div class="buttons">
+                                <div class="buttons pt-2">
                                     <button class="next">下一步</button>
                                     <button class="prev">返回</button>
                                 </div>
@@ -89,7 +89,7 @@
                                 </div>
                             </div>
                             <div class="content">
-                                <div class="buttons">
+                                <div class="buttons pt-2">
                                     <button class="next">下一步</button>
                                     <button class="prev">返回</button>
                                 </div>
@@ -112,41 +112,43 @@
                                 Step 4 - 同意版規
                             </div>
                             <div class="jumbotron jumbotron-fluid rounded p-2 mt-2">
-                                <div class="container">
+                                <div class="container p-0">
                                     <h1 class="text-center w-100 py-2 my-0">內容守則</h1>
-                                    <pre>一、責任聲明：
-本站使用者須對自己所張貼之每一篇文章負責，本站毋需對站內以及其他關聯之社群媒體的言論負擔起任何的責任，責任的歸屬權屬於各位發表人。
-
-二、發表文章時之注意事項：
-1. 尊重他人意見，注意用字遣詞與口氣，避免引起爭吵。
-2. 避免在公眾區域，討論私人事務。
-3. 避免發表文章於非相關區域，文章標題及內容不符合討論區之討論主題。
-4. 禁止重複刊登相同內容或相同意義之留言。
-5. 適度引用文章，避免引用過長文章，造成閱讀困擾。
-6. 不適當的廣告、宣傳活動或商業性留言。
-7. 禁止發表謾罵、脅迫、挑釁、猥褻或不雅之文字。
-8. 禁止發表個人測試用文章或散播不實消息之文章，張貼文章，應自負相關法律責任。
-9. 轉貼任何文章請附上原作者貨來源，否則版主會親自去問授權、處理方式，並且把您的帳號封鎖。
-10. 禁止以發表防疫相關的調侃、反防疫相關言論。
-11. 若有未規定的部份，由版主依主觀認定，視情況處理。
-
-三、違規處理辦法：
-違反上述規定之文章或作者，版主可刪除文章或行使禁貼之處份。
-
-四、附註及補充說明：
-1. 本站歡迎網友互相討論發表己見，唯請務必遵守上述規定。
-2. 是否違反上述規定，由版主主觀認定，請謹慎用詞。
-3. 請學習包容各種意見，如遇惡意批評或攻擊之文章，切勿加入爭執，並且善用檢舉，版主會有適當之處理，否則雙方皆依上述規定處理。</pre>
-                                    <hr>
+                                    <h3>一、責任聲明：</h3>
+                                    <p>本站使用者須對自己所張貼之每一篇文章負責，本站毋需對站內以及其他關聯之社群媒體的言論負擔起任何的責任，責任的歸屬權屬於各位發表人。</p>
+                                    <h3>二、發表文章時之注意事項：</h3>
+                                    <ol class="pb-2">
+                                        <li>尊重他人意見，注意用字遣詞與口氣，避免引起爭吵。</li>
+                                        <li>避免在公眾區域，討論私人事務。</li>
+                                        <li>避免發表文章於非相關區域，文章標題及內容不符合討論區之討論主題。</li>
+                                        <li>禁止重複刊登相同內容或相同意義之留言。</li>
+                                        <li>適度引用文章，避免引用過長文章，造成閱讀困擾。</li>
+                                        <li>不適當的廣告、宣傳活動或商業性留言。</li>
+                                        <li>禁止發表謾罵、脅迫、挑釁、猥褻或不雅之文字。</li>
+                                        <li>禁止發表個人測試用文章或散播不實消息之文章，張貼文章，應自負相關法律責任。</li>
+                                        <li>轉貼任何文章請附上原作者貨來源，否則版主會親自去問授權、處理方式，並且把您的帳號封鎖。</li>
+                                        <li>禁止以發表防疫相關的調侃、反防疫相關言論。</li>
+                                        <li>若有未規定的部份，由版主依主觀認定，視情況處理。</li>
+                                    </ol>
+                                    <h3>三、違規處理辦法：</h3>
+                                    <p>違反上述規定之文章或作者，版主可刪除文章或行使禁貼之處份。</p>
+                                    <h3>四、附註及補充說明：</h3>
+                                    <ol class="pb-2">
+                                        <li>本站歡迎網友互相討論發表己見，唯請務必遵守上述規定。</li>
+                                        <li>是否違反上述規定，由版主主觀認定，請謹慎用詞。</li>
+                                        <li>請學習包容各種意見，如遇惡意批評或攻擊之文章，切勿加入爭執，並且善用檢舉，版主會有適當之處理，否則雙方</li>皆依上述規定處理。
+                                    </ol>
                                 </div>
-                                <input class="tgl tgl-flip" id="consent" type="checkbox" v-model="consent"/>
-                                <label style="display: inline-block;" class="tgl-btn mr-2" data-tg-off="杰哥不要！😱" data-tg-on="好ㄛ🥴" for="consent"></label>
-                                <label style="display: inline-block;" :class="[consent ? 'text-success' : 'text-danger']" for="consent">
-                                    {{ consent ? '我看完了，我願意遵守以上的內容守則，所以我按了「好ㄛ🥴」以表示我同意。' : '杰哥，不要啦！杰哥不要！我不想遵守「內容守則」😭' }}
-                                </label>
-                                <div class="buttons">
-                                    <button class="next" disabled>下一步</button>
-                                    <button class="prev">返回</button>
+                                <div class="mt-2 text-center">
+                                    <input class="tgl tgl-flip" id="consent" type="checkbox" v-model="consent"/>
+                                    <label style="display: inline-block;" class="tgl-btn mt-2 mr-2" data-tg-off="杰哥不要！😱" data-tg-on="好ㄛ🥴" for="consent"></label>
+                                    <label style="display: inline-block;" :class="[consent ? 'text-success' : 'text-danger']" for="consent">
+                                        {{ consent ? '我看完了，我願意遵守以上的內容守則，所以我按了「好ㄛ🥴」以表示我同意。' : '杰哥，不要啦！杰哥不要！我不想遵守「內容守則」😭' }}
+                                    </label>
+                                </div>
+                                <div class="buttons pt-2">
+                                    <button class="next" :disabled="!consent">下一步</button>
+                                    <button class="prev" style="background-color: var(--background-secondary);">返回</button>
                                 </div>
                             </div>
                         </div>
@@ -168,18 +170,15 @@
                             </div>
                             <div class="content">
                                 <div class="inputGroup">
-                                    <p>
-                                        Congratulations! Now you're all set,<br />
-                                        hit that submit button!
-                                    </p>
+                                    <p>好棒，接下來我們只要「送出投稿」就可以了！</p>
                                 </div>
-                                <div class="buttons">
+                                <div class="buttons pt-2">
                                     <button class="submit">送出投稿</button>
                                     <button class="prev">返回</button>
                                 </div>
                                 <div class="message">
-                                    <span class="success">You signed up succesfully!</span>
-                                    <span class="fail">Oops, something went wrong! Please double-check your inputs.</span>
+                                    <span class="success">您成功送出投稿了，</span>
+                                    <span class="fail">啊？好像有些地方不太對勁 ...</span>
                                 </div>
                             </div>
                         </div>
@@ -257,11 +256,13 @@ export default {
 
             // 如果是同意內容守則，內容守則被同意了則啟用下一步按鈕
             if (this.consent) {
+                console.log('同意內容守則');
                 return (nextButton.disabled = false);
             }
 
             // 如果沒有 value，則移除焦點，禁用下一步按鈕並 return
             if (!e.target.value) {
+                console.log('沒有 value');
                 inputWrapper.closest(".listItem").classList.remove("done");
                 inputWrapper.classList.remove("js-focus");
                 return (nextButton.disabled = true);
@@ -270,13 +271,17 @@ export default {
 
             // 如果仍有輸入為空，則禁用下一個按鈕
             if (inputWrapper.nextElementSibling.classList.contains("inputGroup") &&
-               !inputWrapper.nextElementSibling.querySelector("input").value)
-                return;
-
-            if (this.content.length < 30) {
+               !inputWrapper.nextElementSibling.querySelector("input").value) {
+                console.log('輸入為空');
                 return;
             }
 
+            if (this.content.length < 30) {
+                console.log('content.length < 30');
+                return;
+            }
+
+            console.log('啟用下一步按鈕');
             // 否則啟用下一步按鈕
             nextButton.disabled = false;
         },
