d590f2e31fae35d4c5f0ff3027f4d3ea6f9250ce
Kantai <kantai.developer@gmail.com>
2021-09-14 23:44:14 +0800

    新增 tumblr notification

--- app/Domains/Social/Listeners/CardsEventListener.php
+++ app/Domains/Social/Listeners/CardsEventListener.php
@@ -6,6 +6,8 @@ use App\Domains\Social\Events\Cards\ArticleCreated;
 use App\Domains\Social\Events\Cards\PictureCreated;
 use App\Domains\Social\Models\Cards;
 use App\Domains\Social\Models\Platform;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
@@ -136,6 +138,9 @@ class CardsEventListener
                         ),
                     ));
 
+                    /**
+                     * 紀錄 response 資訊
+                     */
                     activity('social cards - discord notification')
                         ->performedOn(Cards::find($data['id']))
                         ->log($response->body());
@@ -145,7 +150,59 @@ class CardsEventListener
                  * 發表到 Tumblr
                  */
                 case Platform::TYPE_TUMBLR:
-                    # code...
+                    /**
+                     * 判斷 Blog Name、Consumer Key、Consumer Secret、Token、Token Secret 是否為空
+                     */
+                    if (!isset($platform->config['user_id']) ||
+                        !isset($platform->config['consumer_app_key']) ||
+                        !isset($platform->config['consumer_app_secret']) ||
+                        !isset($platform->config['access_token']) ||
+                        !isset($platform->config['access_token_secret'])) {
+                        break;
+                    }
+
+                    /**
+                     * 透過 Guzzle 的 HandlerStack 來建立堆疊
+                     */
+                    $stack = HandlerStack::create();
+
+                    /**
+                     * 透過 Guzzle 的 OAuth1 來建立請求
+                     */
+                    $middleware = new Oauth1(array(
+                        'consumer_key' => $platform->config['consumer_app_key'],
+                        'consumer_secret' => $platform->config['consumer_app_secret'],
+                        'token' => $platform->config['access_token'],
+                        'token_secret' => $platform->config['access_token_secret'],
+                    ));
+                    $stack->push($middleware);
+
+                    /**
+                     * 整理 API Uri
+                     */
+                    $name = $platform->config['user_id'];
+                    $url = "/v2/blog/$name.tumblr.com/post";
+
+                    /**
+                     * 開始執行通知
+                     */
+                    $response = Http::withMiddleware($middleware)
+                        ->withOptions(array(
+                            'base_uri' => 'https://api.tumblr.com',
+                            'handler' => $stack,
+                            'auth' => 'oauth',
+                        ))->post($url, array(
+                            'source' => $data['picture'],
+                            'type' => 'photo',
+                            'caption' => Str::limit($desc, 128, '...'),
+                        ));
+
+                    /**
+                     * 紀錄 response 資訊
+                     */
+                    activity('social cards - tumblr notification')
+                        ->performedOn(Cards::find($data['id']))
+                        ->log($response->body());
                     break;
 
                 /**
@@ -160,6 +217,9 @@ class CardsEventListener
                         break;
                     }
 
+                    /**
+                     * 開始執行通知
+                     */
                     $token = $platform->config['access_token'];
                     $url = "https://api.telegram.org/bot$token/sendPhoto";
                     $response = Http::post($url, array(
@@ -168,6 +228,9 @@ class CardsEventListener
                         'caption' => Str::limit($desc, 128, '...'),
                     ));
 
+                    /**
+                     * 紀錄 response 資訊
+                     */
                     activity('social cards - telegram notification')
                         ->performedOn(Cards::find($data['id']))
                         ->log($response->body());
