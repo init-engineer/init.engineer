97aa18a544f8ea28dcc0e887f4f86ed32762be1e
乾太 <kantai.developer@gmail.com>
2021-09-25 13:47:35 +0800

    文章列表的超連結按鈕多一些圖片裝飾

--- resources/views/frontend/social/review.blade.php
+++ resources/views/frontend/social/review.blade.php
@@ -8,6 +8,15 @@
             padding: 2px !important;
             margin: 0px !important;
         }
+        .svg-image {
+            display: block;
+            width: 48px;
+            height: 48px;
+            position: absolute;
+            right: 16px;
+            transform: rotate(36deg);
+            filter: invert(70%) opacity(70%);
+        }
     </style>
 @endpush
 
@@ -29,18 +38,30 @@
             </div><!--col-md-9-->
 
             <div class="col-md-3 order-md-last order-first">
-                <x-utils.link
-                    :href="route('frontend.social.cards.publish.article')"
-                    :text="__('Create Submit')"
-                    class="nav-link btn btn-dos py-3 mb-4" />
-                <x-utils.link
-                    :href="route('frontend.social.cards.publish.picture')"
-                    :text="__('Picture Submit')"
-                    class="nav-link btn btn-dos py-3 mb-4" />
-                <x-utils.link
-                    :href="route('frontend.social.cards.index')"
-                    :text="__('Init.Engineer Submit')"
-                    class="nav-link btn btn-dos py-3 mb-4" />
+                {{-- 文章投稿的按鈕 --}}
+                <div>
+                    <img class="svg-image" src="/img/icon/paper.svg" alt="{{ __('Create Submit') }}" />
+                    <x-utils.link
+                        :href="route('frontend.social.cards.publish.article')"
+                        :text="__('Create Submit')"
+                        class="nav-link btn btn-dos py-3 mb-4" />
+                </div>
+                {{-- 圖片投稿的按鈕 --}}
+                <div>
+                    <img class="svg-image" src="/img/icon/painting.svg" alt="{{ __('Picture Submit') }}" />
+                    <x-utils.link
+                        :href="route('frontend.social.cards.publish.picture')"
+                        :text="__('Picture Submit')"
+                        class="nav-link btn btn-dos py-3 mb-4" />
+                </div>
+                {{-- 文章列表的按鈕 --}}
+                <div>
+                    <img class="svg-image" src="/img/icon/clipboard.svg" alt="{{ __('Init.Engineer Submit') }}" />
+                    <x-utils.link
+                        :href="route('frontend.social.cards.index')"
+                        :text="__('Init.Engineer Submit')"
+                        class="nav-link btn btn-dos py-3 mb-4" />
+                </div>
             </div><!--col-md-3-->
         </div><!--row-->
     </div><!--container-->
