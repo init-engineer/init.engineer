f5789560f0f0a6ec908f3d31897edfb24cce0740
Kantai <kantai.developer@gmail.com>
2021-10-22 10:20:31 +0800

    Companies 建立 Jobs、Members、Links 關聯方法

--- app/Domains/Companie/Models/Traits/Relationship/CompaniesRelationship.php
+++ app/Domains/Companie/Models/Traits/Relationship/CompaniesRelationship.php
@@ -2,6 +2,11 @@
 
 namespace App\Domains\Companie\Models\Traits\Relationship;
 
+use App\Domains\Companie\Models\CompanieJobs;
+use App\Domains\Companie\Models\CompanieLinks;
+use App\Domains\Companie\Models\CompanieMembers;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 /**
  * Trait CompaniesRelationship.
  */
@@ -13,6 +18,36 @@ trait CompaniesRelationship
     public function model()
     {
         return $this->morphTo();
+    }
+
+    /**
+     * Get all of the jobs for the CompaniesRelationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(CompanieJobs::class, 'companie_id', 'id');
+    }
+
+    /**
+     * Get all of the members for the CompaniesRelationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function members(): HasMany
+    {
+        return $this->hasMany(CompanieMembers::class, 'companie_id', 'id');
+    }
+
+    /**
+     * Get all of the links for the CompaniesRelationship
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function links(): HasMany
+    {
+        return $this->hasMany(CompanieLinks::class, 'companie_id', 'id');
     }
 
     /**
