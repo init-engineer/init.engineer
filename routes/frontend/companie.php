9b22eaf4f895df181c77c2c3f1c2180b2767fdcb
Kantai <kantai.developer@gmail.com>
2021-10-22 10:24:31 +0800

    Companie Jobs 建立草稿

--- routes/frontend/companie.php
+++ routes/frontend/companie.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Domains\Companie\Http\Controllers\Frontend\CompanieController;
+use App\Domains\Companie\Http\Controllers\Frontend\DeactivatedCompanieController;
+use App\Domains\Companie\Http\Controllers\Frontend\JobsController;
+use App\Domains\Companie\Models\CompanieJobs;
+use App\Domains\Companie\Models\Companies;
 use Tabuna\Breadcrumbs\Trail;
 
 /*
@@ -11,21 +15,87 @@ Route::group([
     'prefix' => 'companie',
     'as' => 'companie.',
     'namespace' => 'Companie',
-    'middleware' => [
-        'auth',
-    ],
 ], function () {
-    Route::get('/', [CompanieController::class, 'index'])
-        ->name('index')
-        ->breadcrumbs(function (Trail $trail) {
-            $trail->parent('frontend.jobs.index')
-                ->push(__('Init.Engineer Companie'), route('frontend.companie.index'));
-        });
+    Route::group([
+        'middleware' => 'auth',
+    ], function () {
+        Route::get('/', [CompanieController::class, 'index'])
+            ->name('index')
+            ->breadcrumbs(function (Trail $trail) {
+                $trail->parent('frontend.jobs.index')
+                    ->push(__('Init.Engineer Companie'), route('frontend.companie.index'));
+            });
 
-    Route::get('/create', [CompanieController::class, 'create'])
-        ->name('create')
-        ->breadcrumbs(function (Trail $trail) {
-            $trail->parent('frontend.companie.index')
-                ->push(__('Companie Create'), route('frontend.companie.create'));
+        Route::get('/create', [CompanieController::class, 'create'])
+            ->name('create')
+            ->breadcrumbs(function (Trail $trail) {
+                $trail->parent('frontend.companie.index')
+                    ->push(__('Companie Create'), route('frontend.companie.create'));
+            });
+
+        Route::group([
+            'prefix' => '{companie}',
+        ], function () {
+            Route::get('edit', [CompanieController::class, 'edit'])
+                ->name('edit')
+                ->breadcrumbs(function (Trail $trail, Companies $companie) {
+                    $trail->parent('frontend.companie.show', $companie)
+                        ->push(__('Edit'), route('frontend.companie.edit', $companie));
+                });
+
+            Route::patch('mark/{status}', [DeactivatedCompanieController::class, 'update'])
+                ->name('mark')
+                ->where(['status' => '[0,1]']);
+
+            Route::delete('/', [CompanieController::class, 'destroy'])->name('destroy');
+
+            Route::group([
+                'prefix' => 'jobs',
+                'as' => 'jobs.',
+                'namespace' => 'Jobs',
+            ], function () {
+                Route::get('/', [JobsController::class, 'list'])
+                    ->name('index')
+                    ->breadcrumbs(function (Trail $trail, Companies $companie) {
+                        $trail->parent('frontend.companie.show', $companie)
+                            ->push(__('Companie Jobs'), route('frontend.companie.jobs.index', $companie));
+                    });
+
+                Route::get('/create', [JobsController::class, 'create'])
+                    ->name('create')
+                    ->breadcrumbs(function (Trail $trail, Companies $companie) {
+                        $trail->parent('frontend.companie.jobs.index', $companie)
+                            ->push(__('Companie Jobs Create'), route('frontend.companie.jobs.create', $companie));
+                    });
+
+                Route::group([
+                    'prefix' => '{job}',
+                ], function () {
+                    Route::get('edit', [JobsController::class, 'edit'])
+                        ->name('edit')
+                        ->breadcrumbs(function (Trail $trail, Companies $companie, CompanieJobs $job) {
+                            $trail->parent('frontend.jobs.show', $job)
+                                ->push(__('Edit'), route('frontend.companie.jobs.edit', $companie, $job));
+                        });
+
+                    Route::patch('mark/{status}', [DeactivatedJobController::class, 'update'])
+                        ->name('mark')
+                        ->where(['status' => '[0,1]']);
+
+                    Route::delete('/', [JobsController::class, 'destroy'])->name('destroy');
+                });
+            });
         });
+    });
+
+    Route::group([
+        'prefix' => '{companie}',
+    ], function () {
+        Route::get('/', [CompanieController::class, 'show'])
+            ->name('show')
+            ->breadcrumbs(function (Trail $trail, Companies $companie) {
+                $trail->parent('frontend.jobs.index')
+                    ->push($companie->name, route('frontend.companie.show', $companie));
+            });
+    });
 });
--- routes/frontend/jobs.php
+++ routes/frontend/jobs.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Domains\Companie\Http\Controllers\Frontend\JobsController;
+use App\Domains\Companie\Models\CompanieJobs;
 use Tabuna\Breadcrumbs\Trail;
 
 /*
@@ -18,4 +19,15 @@ Route::group([
             $trail->parent('frontend.index')
                 ->push(__('Init.Engineer Jobs'), route('frontend.jobs.index'));
         });
+
+    Route::group([
+        'prefix' => '{job}',
+    ], function () {
+        Route::get('/', [JobsController::class, 'show'])
+            ->name('show')
+            ->breadcrumbs(function (Trail $trail, CompanieJobs $job) {
+                $trail->parent('frontend.jobs.index')
+                    ->push($job->companie()->name . ' - ' . $job->name, route('frontend.jobs.show', $job));
+            });
+    });
 });
