491057445f0d0c01c31477fdae362f51b7d5efe5
乾太 <kantai.developer@gmail.com>
2021-11-15 20:37:48 +0800

    Update SocialCardsTableSeeder.php

--- database/seeders/Social/SocialCardsTableSeeder.php
+++ database/seeders/Social/SocialCardsTableSeeder.php
@@ -25,8 +25,8 @@ class SocialCardsTableSeeder extends Seeder
             Cards::create([
                 'model_type' => User::class,
                 'model_id' => 1,
-                'content' => '這是一篇測試文章，已通過群眾審核，並且沒有橫幅廣告資訊。',
-                'config' => json_encode(array(
+                'content' => "這是一篇測試文章，已通過群眾審核，並且沒有橫幅廣告資訊。",
+                'config' => array(
                     'probability' => 2000,
                     'item' => array(
                         array(
@@ -34,12 +34,12 @@ class SocialCardsTableSeeder extends Seeder
                             'probability' => 1000,
                         ),
                     ),
-                )),
-                'picture' => json_encode(array(
-                    'local' => 'img/default/800x600.png',
+                ),
+                'picture' => array(
+                    'local' => "img/default/800x600.png",
                     'storage' => null,
                     'imgur' => null,
-                )),
+                ),
                 'active' => true,
                 'blockade' => false,
                 'blockade_by' => null,
@@ -50,8 +50,8 @@ class SocialCardsTableSeeder extends Seeder
             Cards::create([
                 'model_type' => User::class,
                 'model_id' => 1,
-                'content' => '這是一篇測試文章，已通過群眾審核，並且包含橫幅廣告資訊。',
-                'config' => json_encode(array(
+                'content' => "這是一篇測試文章，已通過群眾審核，並且包含橫幅廣告資訊。",
+                'config' => array(
                     'probability' => 500,
                     'item' => array(
                         array(
@@ -59,12 +59,12 @@ class SocialCardsTableSeeder extends Seeder
                             'probability' => 1000,
                         ),
                     ),
-                )),
-                'picture' => json_encode(array(
-                    'local' => 'img/default/800x600.png',
+                ),
+                'picture' => array(
+                    'local' => "img/default/800x600.png",
                     'storage' => null,
                     'imgur' => null,
-                )),
+                ),
                 'active' => true,
                 'blockade' => false,
                 'blockade_by' => null,
@@ -75,17 +75,17 @@ class SocialCardsTableSeeder extends Seeder
             Cards::create([
                 'model_type' => User::class,
                 'model_id' => 1,
-                'content' => '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第一行。\n\r' .
-                             '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第二行。\n\r' .
-                             '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第三行。\n\r' .
-                             '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第四行。\n\r' .
-                             '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第五行。\n\r' .
-                             '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第六行。\n\r' .
-                             '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第七行。\n\r' .
-                             '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第八行。\n\r' .
-                             '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第九行。\n\r' .
-                             '這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第十行。',
-                'config' => json_encode(array(
+                'content' => "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第一行。\n\r" .
+                             "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第二行。\n\r" .
+                             "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第三行。\n\r" .
+                             "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第四行。\n\r" .
+                             "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第五行。\n\r" .
+                             "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第六行。\n\r" .
+                             "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第七行。\n\r" .
+                             "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第八行。\n\r" .
+                             "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第九行。\n\r" .
+                             "這是一篇測試文章，已通過群眾審核，不僅包含橫幅廣告資訊，文章內容還相當得長，這是其中的第十行。",
+                'config' => array(
                     'probability' => 500,
                     'item' => array(
                         array(
@@ -93,12 +93,12 @@ class SocialCardsTableSeeder extends Seeder
                             'probability' => 1000,
                         ),
                     ),
-                )),
-                'picture' => json_encode(array(
-                    'local' => 'img/default/800x600.png',
+                ),
+                'picture' => array(
+                    'local' => "img/default/800x600.png",
                     'storage' => null,
                     'imgur' => null,
-                )),
+                ),
                 'active' => true,
                 'blockade' => false,
                 'blockade_by' => null,
@@ -109,8 +109,8 @@ class SocialCardsTableSeeder extends Seeder
             Cards::create([
                 'model_type' => User::class,
                 'model_id' => 1,
-                'content' => '這是一篇測試文章，尚未通過群眾審核，並且沒有橫幅廣告資訊。',
-                'config' => json_encode(array(
+                'content' => "這是一篇測試文章，尚未通過群眾審核，並且沒有橫幅廣告資訊。",
+                'config' => array(
                     'probability' => 2000,
                     'item' => array(
                         array(
@@ -118,12 +118,12 @@ class SocialCardsTableSeeder extends Seeder
                             'probability' => 1000,
                         ),
                     ),
-                )),
-                'picture' => json_encode(array(
-                    'local' => 'img/default/800x600.png',
+                ),
+                'picture' => array(
+                    'local' => "img/default/800x600.png",
                     'storage' => null,
                     'imgur' => null,
-                )),
+                ),
                 'active' => false,
                 'blockade' => false,
                 'blockade_by' => null,
@@ -134,8 +134,8 @@ class SocialCardsTableSeeder extends Seeder
             Cards::create([
                 'model_type' => User::class,
                 'model_id' => 1,
-                'content' => '這是一篇測試文章，尚未通過群眾審核，但是包含橫幅廣告資訊。',
-                'config' => json_encode(array(
+                'content' => "這是一篇測試文章，尚未通過群眾審核，但是包含橫幅廣告資訊。",
+                'config' => array(
                     'probability' => 500,
                     'item' => array(
                         array(
@@ -143,12 +143,12 @@ class SocialCardsTableSeeder extends Seeder
                             'probability' => 1000,
                         ),
                     ),
-                )),
-                'picture' => json_encode(array(
-                    'local' => 'img/default/800x600.png',
+                ),
+                'picture' => array(
+                    'local' => "img/default/800x600.png",
                     'storage' => null,
                     'imgur' => null,
-                )),
+                ),
                 'active' => false,
                 'blockade' => false,
                 'blockade_by' => null,
@@ -159,8 +159,8 @@ class SocialCardsTableSeeder extends Seeder
             Cards::create([
                 'model_type' => User::class,
                 'model_id' => 1,
-                'content' => '這是一篇測試文章，已被發表出去，並且文章遭到封鎖。',
-                'config' => json_encode(array(
+                'content' => "這是一篇測試文章，已被發表出去，並且文章遭到封鎖。",
+                'config' => array(
                     'probability' => 2000,
                     'item' => array(
                         array(
@@ -168,16 +168,16 @@ class SocialCardsTableSeeder extends Seeder
                             'probability' => 1000,
                         ),
                     ),
-                )),
-                'picture' => json_encode(array(
-                    'local' => 'img/default/800x600.png',
+                ),
+                'picture' => array(
+                    'local' => "img/default/800x600.png",
                     'storage' => null,
                     'imgur' => null,
-                )),
+                ),
                 'active' => true,
                 'blockade' => true,
                 'blockade_by' => 1,
-                'blockade_remarks' => '這是一則測試的封鎖文章訊息。',
+                'blockade_remarks' => "這是一則測試的封鎖文章訊息。",
                 'blockade_at' => now(),
             ]);
         }
