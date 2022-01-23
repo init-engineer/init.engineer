fd1c4cd4e089ebf905ce30c8bc7643abfb83827e
乾太 <kantai.developer@gmail.com>
2022-01-23 12:39:06 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,7 @@
 Route::group(['namespace' => 'Api', 'as' => 'api.'], function () {
     includeRouteFiles(__DIR__.'/api/');
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+// });
