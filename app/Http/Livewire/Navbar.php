1828c55fa17d8e9ce2619719e0799020c36e0c04
pan93412 <pan93412@gmail.com>
2022-02-03 17:44:53 +0800

    feat: initiate Navbar

---
+++ app/Http/Livewire/Navbar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Navbar extends Component
+{
+    public function render()
+    {
+        return view('frontend.navbar');
+    }
+}
---
+++ resources/views/frontend/navbar.blade.php
@@ -0,0 +1,3 @@
+<div>
+    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+</div>
