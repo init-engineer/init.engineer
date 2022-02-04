9c64558cc008e8a074b7db9824864628cc741acb
pan93412 <pan93412@gmail.com>
2022-02-04 12:29:05 +0800

    feat(component/navbar/navbar): done

---
+++ resources/views/components/navbar/navbar.blade.php
@@ -0,0 +1,17 @@
+<section class="flex justify-between w-screen gap-5 text-gray-200 navbar">
+    <div class="navbar-title">純靠北工程師</div>
+    <div class="flex gap-1 navbar-elements align-items-center">
+        <x-navbar.item active>首頁</x-navbar.item>
+        <x-navbar.item>投稿</x-navbar.item>
+        <x-navbar.item>審稿</x-navbar.item>
+        <x-navbar.item>找工作</x-navbar.item>
+        <x-navbar.item>找案子</x-navbar.item>
+        <x-navbar.item>找社群</x-navbar.item>
+        <x-navbar.item>找活動</x-navbar.item>
+        <x-navbar.item>找研討會</x-navbar.item>
+        <x-navbar.item>小工具</x-navbar.item>
+    </div>
+    <div class="navbar-user">
+        登入
+    </div>
+</section>
