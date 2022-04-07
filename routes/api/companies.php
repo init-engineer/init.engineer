57abca9a62dcf142149cad694efe843fbfe6f79d
Kantai <kantai.developer@gmail.com>
2022-04-08 01:35:03 +0800

    關閉尚未開發完畢的 Companies API

--- routes/api/companies.php
+++ routes/api/companies.php
@@ -1,26 +1,26 @@
 <?php
 
-use App\Domains\Companie\Http\Controllers\Api\CompaniesController;
+// use App\Domains\Companie\Http\Controllers\Api\CompaniesController;
 
 /**
  * All route names are prefixed with 'api.companies'.
  *
  * 公司相關功能
  */
-Route::group([
-    'prefix' => 'companies',
-    'as' => 'companies.',
-    'namespace' => 'Companies',
-    'middleware' => [
-        'auth',
-    ],
-], function () {
-    Route::post('/', [CompaniesController::class, 'store'])->name('store');
+// Route::group([
+//     'prefix' => 'companies',
+//     'as' => 'companies.',
+//     'namespace' => 'Companies',
+//     'middleware' => [
+//         'auth',
+//     ],
+// ], function () {
+//     Route::post('/', [CompaniesController::class, 'store'])->name('store');
 
-    Route::group([
-        'prefix' => '{companie}',
-    ], function () {
-        Route::patch('/', [CompaniesController::class, 'store'])->name('store');
-        // Route::post('/', [CompanieJobsController::class, 'store'])->name('store');
-    });
-});
+//     Route::group([
+//         'prefix' => '{companie}',
+//     ], function () {
+//         Route::patch('/', [CompaniesController::class, 'store'])->name('store');
+//         Route::post('/', [CompanieJobsController::class, 'store'])->name('store');
+//     });
+// });
