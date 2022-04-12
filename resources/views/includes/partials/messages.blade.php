11bfe7d918282959ea6c46917cf5cda8cfdde0a9
乾太 <Kantai235@users.noreply.github.com>
2022-04-12 23:51:45 +0800

    讓登入時的 alert message 更加明顯

--- resources/views/includes/partials/messages.blade.php
+++ resources/views/includes/partials/messages.blade.php
@@ -1,49 +1,57 @@
 @if(isset($errors) && $errors->any())
-    <x-utils.alert type="danger" class="header-message">
-        @foreach($errors->all() as $error)
-            {{ $error }}<br/>
-        @endforeach
-    </x-utils.alert>
+<x-utils.alert type="danger" class="rounded m-2">
+    <i class="fas fa-exclamation-triangle"></i>
+    @foreach($errors->all() as $error)
+    {{ $error }}<br />
+    @endforeach
+</x-utils.alert>
 @endif
 
 @if(session()->get('flash_success'))
-    <x-utils.alert type="success" class="header-message">
-        {{ session()->get('flash_success') }}
-    </x-utils.alert>
+<x-utils.alert type="success" class="rounded m-2">
+    <i class="fas fa-check-circle"></i>
+    {{ session()->get('flash_success') }}
+</x-utils.alert>
 @endif
 
 @if(session()->get('flash_warning'))
-    <x-utils.alert type="warning" class="header-message">
-        {{ session()->get('flash_warning') }}
-    </x-utils.alert>
+<x-utils.alert type="warning" class="rounded m-2">
+    <i class="fas fa-exclamation-circle"></i>
+    {{ session()->get('flash_warning') }}
+</x-utils.alert>
 @endif
 
 @if(session()->get('flash_info') || session()->get('flash_message'))
-    <x-utils.alert type="info" class="header-message">
-        {{ session()->get('flash_info') }}
-    </x-utils.alert>
+<x-utils.alert type="info" class="rounded m-2">
+    <i class="fas fa-info-circle"></i>
+    {{ session()->get('flash_info') }}
+</x-utils.alert>
 @endif
 
 @if(session()->get('flash_danger'))
-    <x-utils.alert type="danger" class="header-message">
-        {{ session()->get('flash_danger') }}
-    </x-utils.alert>
+<x-utils.alert type="danger" class="rounded m-2">
+    <i class="fas fa-exclamation-triangle"></i>
+    {{ session()->get('flash_danger') }}
+</x-utils.alert>
 @endif
 
 @if(session()->get('status'))
-    <x-utils.alert type="success" class="header-message">
-        {{ session()->get('status') }}
-    </x-utils.alert>
+<x-utils.alert type="success" class="rounded m-2">
+    <i class="fas fa-check-circle"></i>
+    {{ session()->get('status') }}
+</x-utils.alert>
 @endif
 
 @if(session()->get('resent'))
-    <x-utils.alert type="success" class="header-message">
-        @lang('A fresh verification link has been sent to your email address.')
-    </x-utils.alert>
+<x-utils.alert type="success" class="rounded m-2">
+    <i class="fas fa-check-circle"></i>
+    @lang('A fresh verification link has been sent to your email address.')
+</x-utils.alert>
 @endif
 
 @if(session()->get('verified'))
-    <x-utils.alert type="success" class="header-message">
-        @lang('Thank you for verifying your e-mail address.')
-    </x-utils.alert>
+<x-utils.alert type="success" class="rounded m-2">
+    <i class="fas fa-check-circle"></i>
+    @lang('Thank you for verifying your e-mail address.')
+</x-utils.alert>
 @endif
