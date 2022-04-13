dd302a69be5b4881eb9406b1b780afd66037bf49
乾太 <Kantai235@users.noreply.github.com>
2022-04-14 00:29:13 +0800

    切分 Facebook Comments 的排程任務，避免抓取留言時因為遞迴抓取而造成 Memory Overflow 的問題出現

--- app/Domains/Social/Jobs/Comments/FacebookCommentsJob.php
+++ app/Domains/Social/Jobs/Comments/FacebookCommentsJob.php
@@ -4,10 +4,8 @@ namespace App\Domains\Social\Jobs\Comments;
 
 use App\Domains\Social\Models\Platform;
 use App\Domains\Social\Models\PlatformCards;
-use App\Domains\Social\Services\CommentsService;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
-use Illuminate\Container\Container;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
@@ -104,144 +102,8 @@ class FacebookCommentsJob implements ShouldQueue
         }
 
         /**
-         * 換得整個 Comments 的資訊列
+         * 透過呼叫 FacebookCommentJob 來建立遞迴排程，避免 Job 因為遞迴呼叫 API 而造成 memory overflow
          */
-        $comments = $this->getComments(
-            $this->platform->config['graph_version'],
-            $this->platform->config['user_id'],
-            $this->platformCards->platform_string_id,
-            $accessToken,
-        );
-
-        /**
-         * 建構 CommentsService 物件
-         */
-        $container = Container::getInstance();
-        $commentService = $container->make(CommentsService::class);
-
-        /**
-         * 根據 Comments 的資訊列來逐一檢查是否新增或更新的資料
-         */
-        foreach ($comments as $comment) {
-            /**
-             * 判斷 Comment 是否已經存在
-             */
-            if ($commentModel = $commentService->findCommentById($comment['id'])) {
-                /**
-                 * Comment 已經存在，檢查 message 是否有更新過，如果有更新過，就同步更新資料庫內的資料。
-                 */
-                if ($commentModel->content != $comment['message']) {
-                    $commentModel->content = $comment['message'];
-                    $commentModel->save();
-                }
-            } else {
-                /**
-                 * Comment 並不存在，直接寫入一筆新的資料。
-                 */
-                $commentService->store(array(
-                    'card_id' => $this->platformCards->card_id,
-                    'platform_id' => $this->platform->id,
-                    'platform_card_id' => $this->platformCards->id,
-                    'comment_id' => $comment['id'],
-                    'content' => $comment['message'] ?? null,
-                    'created_at' => $comment['created_time'],
-                ));
-            }
-
-            /**
-             * 判斷 Comment 是否有被 Reply
-             */
-            if (isset($comment['comments'])) {
-                foreach ($comment['comments']['data'] as $reply) {
-                    /**
-                     * 判斷 Reply 是否已經存在
-                     */
-                    if ($replyModel = $commentService->findCommentById($reply['id'])) {
-                        /**
-                         * Reply 已經存在，檢查 message 是否有更新過，如果有更新過，就同步更新資料庫內的資料。
-                         */
-                        if ($replyModel->content != $reply['message']) {
-                            $replyModel->content = $reply['message'];
-                            $replyModel->save();
-                        }
-                    } else {
-                        /**
-                         * Reply 並不存在，直接寫入一筆新的資料。
-                         */
-                        $commentService->store(array(
-                            'card_id' => $this->platformCards->card_id,
-                            'platform_id' => $this->platform->id,
-                            'platform_card_id' => $this->platformCards->id,
-                            'comment_id' => $reply['id'],
-                            'content' => $reply['message'] ?? null,
-                            'reply' => $comment['id'],
-                            'created_at' => $reply['created_time'],
-                        ));
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * @param string $graphVersion
-     * @param string $userId
-     * @param string $postId
-     * @param string $accessToken
-     * @param string $after = null
-     * @param array $beforeComments = []
-     *
-     * @return array
-     */
-    private function getComments(string $graphVersion, string $userId, string $postId, string $accessToken, string $after = null, array $beforeComments = []): array
-    {
-        /**
-         * 整理 $url 呼叫的 API URL
-         */
-        $url = sprintf(
-            'https://graph.facebook.com/%s/%s_%s/comments?access_token=%s&limit=25&fields=%s',
-            $graphVersion,
-            $userId,
-            $postId,
-            $accessToken,
-            'created_time,message,id,from{id,name},comments{created_time,message,id,from{id,name}}',
-        );
-
-        if ($after !== null) {
-            $url = sprintf(
-                '%s&after=%s',
-                $url,
-                $after,
-            );
-        }
-
-        /**
-         * 獲得 Comments 資訊
-         */
-        $response = Http::get($url);
-        $responseBody = $response->json();
-
-        /**
-         * 整理 Comments 並判斷是否繼續遞迴去獲取接續的 Comments
-         */
-        if (isset($responseBody['data'])) {
-            $afterComments = $responseBody['data'];
-            $afterComments = array_merge($afterComments, $beforeComments);
-
-            if (isset($responseBody['paging']['next'])) {
-                return $this->getComments(
-                    $graphVersion,
-                    $userId,
-                    $postId,
-                    $accessToken,
-                    $responseBody['paging']['cursors']['after'],
-                    $afterComments,
-                );
-            }
-
-            return $afterComments;
-        }
-
-        return $beforeComments;
+        dispatch(new FacebookCommentJob($this->platform->config['graph_version'], $this->platform->config['user_id'], $this->platformCards->platform_string_id, $accessToken, null));
     }
 }
